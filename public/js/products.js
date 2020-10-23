c32caddf299379274ffb96f7138dfc462e371944
adil <adil.quamar14@gmail.com>
2020-10-22 21:32:44 -0700

    reading and writing to database

--- public/js/products.js
+++ public/js/products.js
@@ -1,4 +1,5 @@
 const sidebar = document.querySelector('.sidenav');
+const prodList = document.querySelector('.products');
 
 const PRODUCT_TEMPLATE =
     '<div class="product">' +
@@ -15,27 +16,32 @@ let CURRENT_PRODUCT = '';
 let PREVIOUS_PRODUCT = '';
 
 sidebar.addEventListener('click', e => {
+    prodList.innerHTML = "";
     console.log(e.target.classList[1]);
     const selectedProduct = e.target.classList[1];
-    document.querySelector(`.display-${selectedProduct}`).classList.add('open');
 
     db.collection(selectedProduct).get().then(querySnapshot => {
         querySnapshot.forEach(doc => {
             const data = doc.data();
-            console.log(data["contact"]);
-            console.log(data["description"]);
-            console.log(data["price"]);
 
-            /*
-            let html = document.querySelector(`.display-${selectedProduct}`);
-            const new_html = `<div class="product" id=`${doc.id}`>
-                        <div class="description"></div>
-                        <div class="image"></div>
-                        <div class="price"></div>
-                        <div class="contact"></div>
-                    </div>`;
+            const html = `
+            <div class="product" id="display-${selectedProduct}">
+                <div class="description">
+                    <a>Description: ${data["description"]}</a>
+                </div>
+                <div class="image">
+                    <img src="images/${selectedProduct}.png" alt="notes default picture">
+                </div>
+                <div class="price">
+                    <a>Price: ${data["price"]}</a>
+                </div>
+                <div class="contact">
+                    <a>Contact: ${data["contact"]}</a>
+                </div>
+            </div>
+            `;
 
-             */
+            prodList.innerHTML += html;
 
         });
     });
